7a7b25a77f7938e39c64753e33b0e508b04f7a5a
Nam Le <nam.le@advesa.com>
2021-03-30 17:09:57 +0700

    Life saver setting. Oh my godness I love this config

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,1 +1,4 @@
 vim.o.termguicolors = true -- set term giu colors most terminals support this
+-- life saver. Get rid of fucking "E37: No write since last change (add ! to override)"
+-- refer: https://vi.stackexchange.com/questions/21885/save-buffer-before-opening-new-file
+vim.o.hidden = true -- Required to keep multiple buffers open multiple buffers
